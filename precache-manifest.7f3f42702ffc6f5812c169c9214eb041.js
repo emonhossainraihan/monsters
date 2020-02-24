dcc92b68772e3f56b0f254e638ce2a91807553c3
emonhossainraihan <mdroy7475@gmail.com>
2020-02-25 02:24:13 +0600

    Updates

---
+++ precache-manifest.7f3f42702ffc6f5812c169c9214eb041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d619208e9cd2c0deee6b4dca2a4eab",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "422521dbb9e001a6d13d",
+    "url": "/monsters/static/css/main.93547979.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422521dbb9e001a6d13d",
+    "url": "/monsters/static/js/main.88917ce1.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
